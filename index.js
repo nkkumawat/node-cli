4a768ccf32712dda52be7266bed02bf02c7c0c6b
nkkumawat <nk0kumawat@gmail.com>
2019-09-24 23:45:38 +0530

    cli colors and spinner

--- index.js
+++ index.js
@@ -80,25 +80,25 @@ module.exports = {
 }
 
 class Spinner {
-  	constructor(color) {
-    	this.color = this.colorToText(color);
-  	}
-  	colorToText(color) {
-		var colorsHash = {
-			"yellow" : '\x1b[43m',
-			"green" :  '\x1b[42m',
-			"blue" : '\x1b[44m',
-			"mgenta" : '\x1b[45m',
-			"cyan" : '\x1b[46m',
-			"white" : '\x1b[47m',
-			"red" : '\x1b[41m'
-		}
-		return colorsHash[color];
+	constructor(color) {
+  	this.color = this.colorToText(color);
 	}
-  	show(percentage) {
-  		if(percentage > 100){
-  			return;
-  		}
+	colorToText(color) {
+	var colorsHash = {
+		"yellow" : '\x1b[43m',
+		"green" :  '\x1b[42m',
+		"blue" : '\x1b[44m',
+		"mgenta" : '\x1b[45m',
+		"cyan" : '\x1b[46m',
+		"white" : '\x1b[47m',
+		"red" : '\x1b[41m'
+	}
+	return colorsHash[color];
+	}
+	show(percentage) {
+		if(percentage > 100){
+			return;
+		}
 		var spaces = Math.floor(percentage);
 		var spc = ""
 		for (var i = 0; i <= spaces; i++) {
